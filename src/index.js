55c654e7d343acab0edc7ee4e74a295a1a454ba0
Jairo Rojas <jairo142014@hotmail.com>
2020-08-04 11:53:42 -0500

    Archivos

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navigation from './Components/Navigation';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navigation/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
